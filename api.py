f4e2dde1901b5a7701333ab68cb32c7cf3c23e6e
thatsrohan <rg2oob@gmail.com>
2026-03-16 00:01:47 +0530

    CORS Workaround

--- api.py
+++ api.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.concurrency import run_in_threadpool
 from pydantic import BaseModel
 
 from Inference import ask
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Query(BaseModel):
     question: str
